ab082d994cbe9520088e4644f6b62943839526a9
LUIZ C G DOS ANJOS <luizanjos88@hotmail.com>
2026-06-18 21:42:08 -0300

    Teste e2e

---
+++ e2e/visitarPaginaInicial.spec.ts
@@ -0,0 +1,13 @@
+import test, { expect } from "@playwright/test";
+
+test.describe('Página Inicial', () => {
+    test('Deve visitar a página inicial', async ({ page }) => {
+        await page.goto('http://localhost:4200');
+        await expect(page).toHaveTitle('Jornada Milhas')
+
+
+
+    })
+
+})
+
