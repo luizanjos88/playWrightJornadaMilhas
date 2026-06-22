23846ec9c97cc718be766c36cfb1ca206717d245
luizanjos88 <luizanjos88@gmail.com>
2026-06-22 12:22:21 -0300

    feat: implement login page object and end-to-end authentication tests

--- e2e/fazerlogin.spec.ts
+++ e2e/fazerlogin.spec.ts
@@ -1,12 +1,22 @@
 import test from "@playwright/test";
 import PaginaLogin from "./page-objects/PaginaLogin";
 
+
 test.describe("Pagina de Login", () => {
     test("Deve conseguir fazer Login com os dados válidos", async ({ page }) => {
         const paginaLogin = new PaginaLogin(page);
 
-        await paginaLogin.visitar()
+        await paginaLogin.visitar();
+        await paginaLogin.fazerLogin('luizanjos88@gmail.com', '123456');
+        await paginaLogin.loginFeitoComSucesso()
+    });
 
+    test("Não deve conseguir fazer login com o email inválido", async ({ page }) => {
+
+        const paginaLogin = new PaginaLogin(page);
+        await paginaLogin.visitar();
+        await paginaLogin.fazerLogin('luizerrado@gmail.com', '123456');
+        await paginaLogin.estaMostrandoMsgDeErro('Você não está autorizado a acessar este recurso');
 
     })
 })
